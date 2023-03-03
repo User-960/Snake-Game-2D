b53abb9eec8efc3b21a8409a7c4bb116fc84a23c
Ivan <ivanvolodin960@gmail.com>
2023-03-04 01:47:51 +0300

    feat: add styles for element form

--- src/modules/game.js
+++ src/modules/game.js
@@ -1,3 +1,12 @@
+import iconLogo from "../images/logo.png";
+
+const gameForm = document.querySelector(".game__form");
+const imgLogo = new Image();
+imgLogo.classList.add("game__logo");
+imgLogo.src = iconLogo;
+imgLogo.alt = "logo";
+gameForm.insertAdjacentElement("afterbegin", imgLogo);
+
 let gameInner = document.querySelector(".game__inner");
 let field = document.createElement("div");
 gameInner.after(field);
@@ -6,7 +15,7 @@ field.classList.add("field");
 let sizeField = 500;
 
 for (let i = 1; i < sizeField + 1; i++) {
-    let excel = document.createElement("div");
-    field.appendChild(excel);
-    excel.classList.add("excel");
-  }
+  let excel = document.createElement("div");
+  field.appendChild(excel);
+  excel.classList.add("excel");
+}
